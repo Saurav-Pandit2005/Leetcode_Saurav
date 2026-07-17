056b7a17e738fe4f5679a06d2ca863ed42752273
Saurav Pandit <sauravpandit9691@gmail.com>
2026-07-17 16:37:39 +0530

    Time: 0 ms (100%), Space: 44 MB (43.84%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,11 +1,21 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0; i<matrix.length; i++) {
-            for(int j=0; j<matrix[0].length; j++) {
-                if(matrix[i][j] == target) {
-                    return true;
-                }
-            }
+        // Brute Force - O(n^2)
+        // for(int i=0; i<matrix.length; i++) {
+        //     for(int j=0; j<matrix[0].length; j++) {
+        //         if(matrix[i][j] == target) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        // StairCase Search - O(n + m)
+        int row = 0, column = matrix[0].length-1;
+        while(row < matrix.length && column >= 0) {
+            if(matrix[row][column] == target) return true;
+            else if (matrix[row][column] < target) row++;
+            else column--; 
         }
         return false;
     }

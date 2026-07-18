503aa7835c71a0da5b7f7cce20d43289e71b3d35
Saurav Pandit <sauravpandit9691@gmail.com>
2026-07-18 20:53:38 +0530

    Time: 1 ms (99.9%), Space: 55.7 MB (46.77%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,15 +2,15 @@ class Solution {
     public int majorityElement(int[] nums) {
         // Using Hashing
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) +1);
-        }
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int num : nums) {
+        //     map.put(num, map.getOrDefault(num, 0) +1);
+        // }
 
-        for(int freq : map.keySet()) {
-            if(map.get(freq) > nums.length/2) return freq;
-        }
-        return -1;
+        // for(int freq : map.keySet()) {
+        //     if(map.get(freq) > nums.length/2) return freq;
+        // }
+        // return -1;
 
         // 2. Using Sorting
 
@@ -19,12 +19,12 @@ class Solution {
 
         // 3. Using Array - Moore Voting Algorithm
 
-        // int freq = 0, ans = 0;
-        // for(int i=0; i<nums.length; i++) {
-        //     if(freq == 0) ans = nums[i];
-        //     if(ans == nums[i]) freq++;
-        //     else freq--;
-        // }
-        // return ans;
+        int freq = 0, ans = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(freq == 0) ans = nums[i];
+            if(ans == nums[i]) freq++;
+            else freq--;
+        }
+        return ans;
     }
 }

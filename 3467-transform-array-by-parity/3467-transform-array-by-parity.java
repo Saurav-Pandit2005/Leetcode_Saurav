75392daaa1eafad775e16706885c9db69556624b
Saurav Pandit <sauravpandit9691@gmail.com>
2026-08-02 00:03:00 +0530

    Time: 1 ms (99.73%), Space: 46.8 MB (63.96%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.java
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -2,11 +2,32 @@ class Solution {
     public int[] transformArray(int[] nums) {
         // Using Sorting    -   O(n log n)
 
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] % 2 == 0) nums[i] = 0;
-            else nums[i] = 1;
+        // for(int i=0; i<nums.length; i++) {
+        //     if(nums[i] % 2 == 0) nums[i] = 0;
+        //     else nums[i] = 1;
+        // }
+        // Arrays.sort(nums);
+
+        // return nums;
+
+
+        // using Array - O(n)
+
+        int even = 0;
+
+        for(int num : nums) {
+            if(num % 2 == 0) {
+                even++;
+            }
         }
-        Arrays.sort(nums);
+
+        for(int i=0; i<nums.length; i++) {
+            if(i < even) {
+                nums[i] = 0;
+            } else {
+                nums[i] = 1;
+            }
+        }
 
         return nums;
     }

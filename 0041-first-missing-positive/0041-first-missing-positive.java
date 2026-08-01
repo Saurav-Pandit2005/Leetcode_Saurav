37c88d3ecddbcb13511fc6fac56fa757c2596047
Saurav Pandit <sauravpandit9691@gmail.com>
2026-08-01 23:08:00 +0530

    Time: 1 ms (99.96%), Space: 71.3 MB (47.98%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -2,18 +2,53 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         // Using HashSet    -> TC - O(n)     -> SC - O(n)
 
-        HashSet<Integer> set = new HashSet<>();
-        for(int num : nums) {
-            set.add(num);
-        }
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int num : nums) {
+        //     set.add(num);
+        // }
 
-        int n = nums.length+1; 
+        // int n = nums.length+1; 
 
-        for(int i=1; i<=n; i++) {
-            if(!set.contains(i)) {
-                return i;
+        // for(int i=1; i<=n; i++) {
+        //     if(!set.contains(i)) {
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // Using pige on hole Principal     -> TC - O(n)        -> SC = O(1)
+
+        // data filtering
+
+        int n = nums.length;
+        
+        for(int i=0; i<n; i++) {
+            if(nums[i] <= 0 || nums[i] >= (n+1)) {
+                nums[i] = n+1;
             }
         }
-        return -1;
+
+        // mark visited
+
+        for(int i=0; i<n; i++) {
+            int element = Math.abs(nums[i]);
+            if(element == n+1) {
+                continue;
+            }
+            int seat = element - 1;
+            if(nums[seat] > 0) {
+                nums[seat] = -nums[seat];
+            }
+        }
+
+        // find first positive value
+
+        for(int i=0; i<n; i++) {
+            if(nums[i] > 0) {
+                return i+1;
+            }
+        }
+
+        return n+1;
     }
 }

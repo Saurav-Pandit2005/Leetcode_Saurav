37ecd715ca7c868fbc6b667058130420361c2a1f
Saurav Pandit <sauravpandit9691@gmail.com>
2026-08-03 14:28:09 +0530

    Time: 12 ms (49.88%), Space: 117.3 MB (20.82%) - LeetHub

--- 3115-maximum-prime-difference/3115-maximum-prime-difference.java
+++ 3115-maximum-prime-difference/3115-maximum-prime-difference.java
@@ -20,7 +20,7 @@ class Solution {
 
     public static boolean isPrime(int n) {
         if(n <= 1) return false;
-        for(int i=2; i<n; i++) {
+        for(int i=2; i*i <= n; i++) {
             if(n%i == 0) return false;
         }
         return true;

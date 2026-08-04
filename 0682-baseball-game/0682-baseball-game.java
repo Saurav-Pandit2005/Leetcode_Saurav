851ef7cfc88fee64a7f187bbdaf51a37387c14bf
Saurav Pandit <sauravpandit9691@gmail.com>
2026-08-04 11:23:36 +0530

    Time: 2 ms (96.56%), Space: 42.8 MB (99.81%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,6 +1,9 @@
 class Solution {
     public int calPoints(String[] operations) {
+        // Using ArrayList
+
         List<Integer> list = new ArrayList<>();
+
         for(String op : operations) {
             if(op.equals("C")){
                 list.remove(list.size()-1);
@@ -19,5 +22,38 @@ class Solution {
         }
 
         return sum;
+
+        // Using Stack
+
+        // Stack<Integer> stack = new Stack<>();
+
+        // for(String op : operations) {
+        //     if(op.equals("C")) {
+        //         stack.pop();
+        //     } 
+        //     else if(op.equals("D")) {
+        //         stack.push(2 * stack.peek());
+        //     } 
+        //     else if(op.equals("+")) {
+        //         // find find n-1 & n-2 for sum
+
+        //         int top = stack.pop();                // n-1
+        //         int second = stack.peek();            // n-2
+
+        //         stack.push(top);                      // push n-1 bcz.. before pop this element.
+        //         stack.push(top + second);
+        //     } 
+        //     else {
+        //         stack.push(Integer.parseInt(op));
+        //     }
+        // }
+
+        // int sum = 0;
+
+        // for(int num : stack) {
+        //     sum += num;
+        // }
+
+        // return sum;
     }
 }

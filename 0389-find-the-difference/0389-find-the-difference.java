d6946103fad65b4d3e63e805c56f042a034b28aa
Saurav Pandit <sauravpandit9691@gmail.com>
2026-08-31 13:50:55 +0530

    Time: 3 ms (42.13%), Space: 42.8 MB (93.12%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,6 +1,5 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-
         int sumS = 0;
         int sumT = 0;
 
@@ -11,7 +10,6 @@ class Solution {
         for(int i=0; i<t.length(); i++) {
             sumT += t.charAt(i);
         }
-
         return (char) (sumT - sumS);
     }
 }

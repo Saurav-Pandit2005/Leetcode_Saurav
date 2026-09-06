c4886d014d43c2644a5027c4db9d9cee2919249b
Saurav Pandit <sauravpandit9691@gmail.com>
2026-09-06 21:26:14 +0530

    Time: 3 ms (86.05%), Space: 43 MB (92.99%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,15 @@
 class Solution {
     public boolean isValid(String s) {
-
         Stack<Character> stack = new Stack();
-
         for(int i=0; i<s.length(); i++) {
             char ch = s.charAt(i);
             if(ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
             }
             else {
-
                 if(stack.isEmpty()) {
                     return false;
                 } 
-                
                 if((stack.peek() == '(' && ch == ')') || 
                    (stack.peek() == '{' && ch == '}') || 
                    (stack.peek() == '[' && ch == ']')) {
@@ -26,11 +22,9 @@ class Solution {
                 }
             }
         }
-
         if(stack.isEmpty()) {
             return true;
         }
-
         return false;
     }
 }
